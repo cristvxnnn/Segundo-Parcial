b017115a44f03813d123ee0433a945b078a73596
cristvxnnnn <cristian.torres_m@uao.edu.co>
2026-04-13 10:46:08 -0500

    hice la  clase madre Persona con datos básicos(nombre, direccion, telefono y fecha de nacimiento) y encapsulamiento

--- src/main/java/Modelo/Persona.java
+++ src/main/java/Modelo/Persona.java
@@ -8,6 +8,57 @@ package Modelo;
  *
  * @author crstc
  */
-public class Persona {
+public abstract class Persona {
+
+    protected String nombre;
+    protected String direccion;
+    protected String telefono;
+    protected Fecha fechaNacimiento;
+
+    public Persona(String nombre, String direccion, String telefono, Fecha fechaNacimiento) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Fecha getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Fecha f) {
+        this.fechaNacimiento = f;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: "+nombre+": \nDireccion: " + direccion + 
+               "\nTelefono: "+telefono+ "\nFecha nacimiento: "+fechaNacimiento; // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
     
+
 }
