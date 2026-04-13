72228de29022f00c38902710acef770c9214fc4c
cristvxnnnn <cristian.torres_m@uao.edu.co>
2026-04-13 11:38:37 -0500

    creación de clase Colegio con listas de profesores y estudiantes; ademas de la implementación de métodos para agregar profesores y estudiantes al colegio

--- src/main/java/Modelo/Colegio.java
+++ src/main/java/Modelo/Colegio.java
@@ -3,14 +3,29 @@
  */
 
 package Modelo;
+import java.util.List;
+import java.util.ArrayList;
 
 /**
  *
  * @author crstc
  */
 public class Colegio {
+    private List<Profesor> profesores;
+    private List<Estudiante> estudiantes;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Colegio() {
+        this.profesores = new ArrayList<>();
+        this.estudiantes = new ArrayList<>();
     }
+    public void agregarProfesor(Profesor p){
+        profesores.add(p);
+        
+    }
+    public void agregarEstudiantes(Estudiante e){
+        estudiantes.add(e);
+    }
+
+    
 }
+    
