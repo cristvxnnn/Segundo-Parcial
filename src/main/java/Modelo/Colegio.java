d969edd8ef05b0c22ace6ce36f822d9c87022efd
cristvxnnnn <cristian.torres_m@uao.edu.co>
2026-04-13 11:39:24 -0500

    se implementa el reporte de estudiantes usando recorrido de lista y taambien el reporte de profesores ordenados por salario y calculo de prestaciones

--- src/main/java/Modelo/Colegio.java
+++ src/main/java/Modelo/Colegio.java
@@ -25,6 +25,41 @@ public class Colegio {
     public void agregarEstudiantes(Estudiante e){
         estudiantes.add(e);
     }
+        public String reporteEstudiantes(){
+        String reporte = "===Lista Estudiantes===\n\n";
+        for(Estudiante e:estudiantes){
+            reporte+= e.toString()+"\n\n";
+        }
+        return reporte;
+    }
+    public String reporteProfesorPorSalario(){
+        List<Profesor> ordenados= new ArrayList<>(profesores);
+        for (int i=0; i<ordenados.size()-1;i++){
+            for (int j=i+1; j<ordenados.size();j++){
+                if(ordenados.get(i).calcularPagoMensual()<ordenados.get(j).calcularPagoMensual()){
+                    Profesor temp= ordenados.get(i);
+                    ordenados.set(i, ordenados.get(j));
+                    ordenados.set(j,temp);
+                }
+            }
+        }
+        String reporte="=== Lista Profesores por salario ===\n\n";
+        double totalPrestaciones=0;
+        for (Profesor p: ordenados){
+            reporte+=p.toString()+"\n\n";
+            totalPrestaciones += p.calcularPrestaciones();
+        }
+        reporte += "Total prestaciones totales: "+ totalPrestaciones;
+        return reporte;
+    }
+
+    public List<Profesor> getProfesores() {
+        return profesores;
+    }
+
+    public List<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
 
     
 }
